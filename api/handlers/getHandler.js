58daf3a404618184a0c1712daaadb669e2eeb332
Dima <harleydiminson@gmail.com>
2025-04-29 19:52:18 +0200

    Update

--- api/handlers/getHandler.js
+++ api/handlers/getHandler.js
@@ -1,15 +1,14 @@
 import { pool } from "../utils/db.js";
 //GetHandler
 export async function getHandler(req, res) {
-    try {
-      const result = await pool.query('SELECT * FROM todo');
-      console.log('GetHandler result:', result.rows);
-      res.status(202).json({
-        message: 'Success',
-        todos: result.rows
-      });
-    } catch (err) {
-      console.error('DB error:', err);
-      res.status(500).json({ error: err.message });
-    }
+  try {
+    const result = await pool.query('SELECT * FROM todo');
+    console.log('GetHandler result:', result.rows);
+    res.status(200).json(
+      result.rows
+    );
+  } catch (err) {
+    console.error('DB error:', err);
+    res.status(500).json({ error: err.message });
   }
+}
--- api/index.js
+++ api/index.js
@@ -4,6 +4,14 @@ import { putHandler } from './handlers/putHadler.js';
 import { deleteHandler } from './handlers/deleteHandler.js';
 
 export default async function handler(req, res) {
+  // Разрешаем CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   console.log(`Request received, method: ${req.method}, url: ${req.url}`);
   res.setHeader('Content-Type', 'application/json');
   switch (req.method) {
